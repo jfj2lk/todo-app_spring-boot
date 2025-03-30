59e6ef19849cc9f2da7ea14defa26a161edc7410
jfj2lk <parasol.sut.crt@gmail.com>
2025-03-30 15:34:06 +0900

    🛠️ Todoモデルクラスにデフォルトコンストラクタ設定

--- spring/src/main/java/app/model/Todo.java
+++ spring/src/main/java/app/model/Todo.java
@@ -11,10 +11,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
