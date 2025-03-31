e062ab0fab33c6eff28d11b52fc5c438ca899bf4
jfj2lk <parasol.sut.crt@gmail.com>
2025-03-31 18:24:05 +0900

    ♻️ 処理が分かりやすくなるようにリファクタリング

--- spring/src/main/java/app/controller/TodoController.java
+++ spring/src/main/java/app/controller/TodoController.java
@@ -1,8 +1,5 @@
 package app.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import app.form.todo.AddTodoForm;
 import app.model.Todo;
 import app.service.TodoService;
@@ -15,6 +12,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
@@ -25,11 +24,15 @@ public class TodoController {
         this.todoService = todoService;
     }
 
+    // 全てのTodo取得
     @GetMapping("/todos")
     public ResponseEntity<Map<String, Iterable<Todo>>> getAllTodos() {
-        return ResponseEntity.ok().body(Map.of("data", todoService.getAllTodos()));
+        // DBから全てのTodoを取得し、その結果を返す
+        Iterable<Todo> todos = todoService.getAllTodos();
+        return ResponseEntity.ok().body(Map.of("data", todos));
     }
 
+    // Todo追加
     @PostMapping("/todos")
     public ResponseEntity<Map<String, Object>> addTodo(@Validated @RequestBody AddTodoForm addTodoForm,
             BindingResult bindingResult) {
