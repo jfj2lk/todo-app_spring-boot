7bdbad4c686e469d2668db09ebc1e68671c03e3d
jfj2lk <parasol.sut.crt@gmail.com>
2025-03-31 17:13:34 +0900

    🪛 Todo追加欄作成

---
+++ vite/src/pages/home/AddTodo.tsx
@@ -0,0 +1,55 @@
+import { ApiResponse } from "@/types/api";
+import { Todo, TodoReducerActions } from "@/types/todo";
+import { apiRequest } from "@/utils/api";
+import { useState } from "react";
+
+const AddTodo = (props: {
+  todos: Todo[];
+  todoDispatch: React.Dispatch<TodoReducerActions>;
+}) => {
+  const [todoName, setTodoName] = useState<string>("");
+  const [todoDesc, setTodoDesc] = useState<string>("");
+
+  // Todo追加
+  const handleAddTodo = async () => {
+    const json = await apiRequest<ApiResponse<Todo>>("/api/todos", "POST", {
+      todo: { name: todoName, desc: todoDesc },
+    });
+    props.todoDispatch({ type: "added", data: json.data });
+    setTodoName("");
+    setTodoDesc("");
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleAddTodo();
+        }}
+      >
+        {/* 名前入力欄 */}
+        <input
+          type="text"
+          value={todoName}
+          onChange={(e) => {
+            setTodoName(e.target.value);
+          }}
+        />
+
+        {/* 詳細入力欄 */}
+        <input
+          type="text"
+          value={todoDesc}
+          onChange={(e) => {
+            setTodoDesc(e.target.value);
+          }}
+        />
+
+        <button>追加</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddTodo;
