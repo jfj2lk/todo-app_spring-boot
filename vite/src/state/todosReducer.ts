fd26eb6fd456e5bb90c2ab7d70fe4a8825d39f0f
jfj2lk <parasol.sut.crt@gmail.com>
2025-03-31 17:07:49 +0900

    🪛 Todoのリデューサ関数作成

---
+++ vite/src/state/todosReducer.ts
@@ -0,0 +1,29 @@
+import { Todo, TodoReducerActions } from "../types/todo";
+
+const todosReducer = (todos: Todo[], action: TodoReducerActions): Todo[] => {
+  switch (action.type) {
+    case "initialized": {
+      return action.data;
+    }
+
+    case "added": {
+      return [...todos, action.data];
+    }
+
+    case "updated": {
+      return todos.map((todo) =>
+        todo.id === action.data.id ? action.data : todo
+      );
+    }
+
+    case "deleted": {
+      return todos.filter((todo) => todo.id !== action.id);
+    }
+
+    default: {
+      throw Error("未定義のアクション");
+    }
+  }
+};
+
+export { todosReducer };
