c654457f39c575e5b7ae55b46e7194cc153c12b7
jfj2lk <parasol.sut.crt@gmail.com>
2025-03-31 14:54:34 +0900

    🛠️ Todo追加用のコントローラー処理追加

--- spring/src/main/java/app/controller/TodoController.java
+++ spring/src/main/java/app/controller/TodoController.java
@@ -3,9 +3,13 @@ package app.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.form.todo.AddTodoForm;
 import app.model.Todo;
 import app.service.TodoService;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,5 +26,17 @@ public class TodoController {
     @GetMapping("/todos")
     public Iterable<Todo> getAllTodos() {
         return todoService.getAllTodos();
+    }
+
+    @PostMapping("/todos")
+    public Object addTodo(@Validated @RequestBody AddTodoForm addTodoForm, BindingResult bindingResult) {
+        // バリデーションエラーが発生した場合は、バリデーションエラーを返す
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
+        }
+
+        // DBにフォームから送信されたTodoデータを保存し、その結果を返す
+        Todo addedTodo = todoService.addTodo(addTodoForm);
+        return addedTodo;
     }
 }
