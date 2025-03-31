d47ce49270a60470329629305a634ca53c0ce92f
jfj2lk <parasol.sut.crt@gmail.com>
2025-03-31 17:29:32 +0900

    🪛 Todo削除ボタンコンポーネント作成

---
+++ vite/src/pages/home/DeleteTodo.tsx
@@ -0,0 +1,33 @@
+import { ApiResponse } from "@/types/api";
+import { Todo, TodoReducerActions } from "@/types/todo";
+import { apiRequest } from "@/utils/api";
+
+const DeleteTodo = (props: {
+  todo: Todo;
+  todos: Todo[];
+  todoDispatch: React.Dispatch<TodoReducerActions>;
+}) => {
+  // Todo削除
+  const handleDeleteTodo = async (deleteTodoId: number) => {
+    const json = await apiRequest<ApiResponse<Todo>>(
+      `/api/todos/${deleteTodoId}`,
+      "DELETE"
+    );
+    props.todoDispatch({ type: "deleted", id: json.data });
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleDeleteTodo(props.todo.id);
+        }}
+      >
+        <button>delete</button>
+      </form>
+    </div>
+  );
+};
+
+export default DeleteTodo;
