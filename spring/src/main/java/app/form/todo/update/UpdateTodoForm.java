0d2600dc8b0bd66a242da6c844068d48d59ae8e4
jfj2lk <parasol.sut.crt@gmail.com>
2025-04-01 09:05:39 +0900

    🛠️ Todo更新用フォームでオブジェクトプロパティに対してもバリデーションを適用するよう修正

--- spring/src/main/java/app/form/todo/update/UpdateTodoForm.java
+++ spring/src/main/java/app/form/todo/update/UpdateTodoForm.java
@@ -1,10 +1,12 @@
 package app.form.todo.update;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UpdateTodoForm {
+    @Valid
     @NotNull(message = "todoは必須です")
     private UpdateTodoInput todo;
 }
