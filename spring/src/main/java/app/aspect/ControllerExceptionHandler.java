f719f4b96c3081cf4b974671f125ca8f5c8255e1
jfj2lk <parasol.sut.crt@gmail.com>
2025-04-02 22:52:47 +0900

    🛠️ コントローラーの例外処理に対応するAOPの作成

---
+++ spring/src/main/java/app/aspect/ControllerExceptionHandler.java
@@ -0,0 +1,19 @@
+package app.aspect;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    /**
+     * 未処理の例外処理
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleGeneralException(Exception e) {
+        return ResponseEntity.internalServerError().body(Map.of("message", e.getMessage()));
+    }
+}
