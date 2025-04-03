f2b3bf91d0eb5eb7d63a91cccebd288e2eccc50d
jfj2lk <parasol.sut.crt@gmail.com>
2025-04-03 09:03:07 +0900

    🛠️ Todoを削除するサービスクラスの処理を作成

--- spring/src/main/java/app/controller/TodoController.java
+++ spring/src/main/java/app/controller/TodoController.java
@@ -4,13 +4,13 @@ import app.form.todo.add.AddTodoForm;
 import app.form.todo.update.UpdateTodoForm;
 import app.model.Todo;
 import app.service.TodoService;
-import jakarta.persistence.EntityNotFoundException;
 
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -48,10 +48,19 @@ public class TodoController {
 
     // Todo更新
     @PatchMapping("/todos/{id}")
-    public ResponseEntity<Map<String, Todo>> updateTodo(@PathVariable("id") Long id, @Validated @RequestBody UpdateTodoForm updateTodoForm,
+    public ResponseEntity<Map<String, Todo>> updateTodo(@PathVariable("id") Long id,
+            @Validated @RequestBody UpdateTodoForm updateTodoForm,
             BindingResult bindingResult) {
         // フォームから送られたTodoデータで更新し、その結果を返す
         Todo updatedTodo = todoService.updateTodo(id, updateTodoForm);
         return ResponseEntity.ok().body(Map.of("data", updatedTodo));
+    }
+
+    // Todo削除
+    @DeleteMapping("/todos/{id}")
+    public ResponseEntity<Map<String, Long>> deleteTodo(@PathVariable("id") Long id) {
+        // パスパラメータのIDのTodoを削除
+        Long deletedTodoId = todoService.deleteTodo(id);
+        return ResponseEntity.ok(Map.of("data", deletedTodoId));
     }
 }
