d29aaf3e1410a0111107ddcf3f04a42f195f5b77
jfj2lk <parasol.sut.crt@gmail.com>
2025-04-05 17:33:13 +0900

    🐛 引数無しのコンストラクタが存在しなかった為、引数無しのコンストラクタを追加

--- spring/src/main/java/app/form/todo/update/UpdateTodoForm.java
+++ spring/src/main/java/app/form/todo/update/UpdateTodoForm.java
@@ -4,9 +4,11 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UpdateTodoForm {
     @Valid
     @NotNull(message = "todoは必須です")
