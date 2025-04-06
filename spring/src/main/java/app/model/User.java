00f2cc206c277b44edba7c85892c8dd8504bd51f
jfj2lk <parasol.sut.crt@gmail.com>
2025-04-06 11:38:26 +0900

    🛠️ Userモデルの作成

---
+++ spring/src/main/java/app/model/User.java
@@ -0,0 +1,44 @@
+package app.model;
+
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, length = 255)
+    private String name;
+
+    @NotBlank
+    @Column(nullable = false, length = 255)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+}
