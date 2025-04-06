0de0bb6088e23e35c38ebbfbfc63e74ae67b8d22
jfj2lk <parasol.sut.crt@gmail.com>
2025-04-06 09:11:12 +0900

    🪛 ホームコンポーネントの内容修正

--- vite/src/pages/home/Home.tsx
+++ vite/src/pages/home/Home.tsx
@@ -1,32 +1,12 @@
-import { todosReducer } from "@/state/todosReducer";
-import { Todo } from "@/types/todo";
-import { apiRequest } from "@/utils/api";
-import { useEffect, useReducer } from "react";
-import AddTodo from "./AddTodo";
-import TodoList from "./TodoList";
+import { Link } from "react-router-dom";
 
-function Home() {
-  const [todos, todoDispatch] = useReducer(todosReducer, []);
-
-  // Todos取得
-  useEffect(() => {
-    (async () => {
-      const json = await apiRequest<Todo[]>("/api/todos");
-      todoDispatch({ type: "initialized", data: json.data });
-    })();
-  }, []);
-
+const Home = () => {
   return (
     <div>
-      <h1>Todo</h1>
-
-      {/* Todo追加欄 */}
-      <AddTodo todos={todos} todoDispatch={todoDispatch} />
-
-      {/* Todo一覧 */}
-      <TodoList todos={todos} todoDispatch={todoDispatch} />
+      <Link to={"/signup"}>新規登録</Link>
+      <Link to={"/login"}>ログイン</Link>
     </div>
   );
-}
+};
 
 export default Home;
