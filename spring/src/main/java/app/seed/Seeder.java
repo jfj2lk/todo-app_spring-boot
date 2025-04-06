407aa8287c10c4783b44fbf1822307600c948082
jfj2lk <parasol.sut.crt@gmail.com>
2025-04-06 11:53:47 +0900

    🛠️ Userシーダーファイルの登録

--- spring/src/main/java/app/seed/Seeder.java
+++ spring/src/main/java/app/seed/Seeder.java
@@ -3,16 +3,17 @@ package app.seed;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Component
-public class Seeder implements CommandLineRunner {
-    private TodoSeeder todoSeeder;
+import lombok.AllArgsConstructor;
 
-    public Seeder(TodoSeeder todoSeeder) {
-        this.todoSeeder = todoSeeder;
-    }
+@Component
+@AllArgsConstructor
+public class Seeder implements CommandLineRunner {
+    private final TodoSeeder todoSeeder;
+    private final UserSeeder userSeeder;
 
     @Override
     public void run(String... args) throws Exception {
+        userSeeder.seedInitialUser();
         todoSeeder.seedInitialTodo();
     }
 }
