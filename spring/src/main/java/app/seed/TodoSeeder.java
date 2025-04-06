39858766565dfbb74a202977bc0e3878d51dbf7d
jfj2lk <parasol.sut.crt@gmail.com>
2025-04-06 11:51:49 +0900

    ♻️ Todoシーダーのリファクタリング

--- spring/src/main/java/app/seed/TodoSeeder.java
+++ spring/src/main/java/app/seed/TodoSeeder.java
@@ -4,19 +4,20 @@ import org.springframework.stereotype.Component;
 
 import app.model.Todo;
 import app.repository.TodoRepository;
+import lombok.AllArgsConstructor;
 
 @Component
+@AllArgsConstructor
 public class TodoSeeder {
 
-    private TodoRepository todoRepository;
+    private final TodoRepository todoRepository;
 
-    public TodoSeeder(TodoRepository todoRepository) {
-        this.todoRepository = todoRepository;
-    }
-
+    /**
+     * Todoの初期データをシードする
+     */
     public void seedInitialTodo() {
-        Todo todo1 = new Todo("todo1", "desc");
-        Todo todo2 = new Todo("todo2", "desc");
+        final Todo todo1 = new Todo("todo1", "desc");
+        final Todo todo2 = new Todo("todo2", "desc");
         todoRepository.save(todo1);
         todoRepository.save(todo2);
     }
