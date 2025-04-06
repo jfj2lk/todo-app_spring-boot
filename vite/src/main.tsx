d3ff767b3f77c8a230c525ba17f46dff54d5c82b
jfj2lk <parasol.sut.crt@gmail.com>
2025-04-06 09:22:38 +0900

    🪛 Signupコンポーネントのルーティング設定

--- vite/src/main.tsx
+++ vite/src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import "@/global.css";
 import Home from "@/pages/home/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./pages/auth/Signup";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
