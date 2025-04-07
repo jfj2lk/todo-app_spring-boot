4189f269a9d39a69c7c10a96bfeb53b70f67ecfc
jfj2lk <parasol.sut.crt@gmail.com>
2025-04-07 14:19:52 +0900

    🛠️ 指定のemailのユーザーを取得する関数の作成

--- spring/src/main/java/app/repository/UserRepository.java
+++ spring/src/main/java/app/repository/UserRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import app.model.User;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
