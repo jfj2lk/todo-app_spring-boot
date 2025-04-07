1193d6f3a7fdb627be4c388b7b621e75a0263ce9
jfj2lk <parasol.sut.crt@gmail.com>
2025-04-08 06:58:25 +0900

    🛠️ ログインフォームの作成

---
+++ spring/src/main/java/app/form/user/login/LoginForm.java
@@ -0,0 +1,18 @@
+package app.form.user.login;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginForm {
+
+    @Valid
+    @NotNull(message = "userは必須です")
+    private LoginInput user;
+
+}
---
+++ spring/src/main/java/app/form/user/login/LoginInput.java
@@ -0,0 +1,20 @@
+package app.form.user.login;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginInput {
+    @NotBlank
+    @Column(nullable = false, length = 255)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false, length = 255)
+    private String password;
+}
