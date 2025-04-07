97d8e25737f738a1f4207c6ea74847d6d3318cda
jfj2lk <parasol.sut.crt@gmail.com>
2025-04-08 07:00:50 +0900

    🛠️ メソッドの作成 ・メールアドレスとパスワードが一致するユーザーを取得する。

--- spring/src/main/java/app/service/UserService.java
+++ spring/src/main/java/app/service/UserService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import app.form.user.add.AddUserForm;
+import app.form.user.login.LoginForm;
+import app.form.user.login.LoginInput;
 import app.model.User;
 import app.repository.UserRepository;
 import jakarta.persistence.EntityExistsException;
@@ -15,6 +17,17 @@ import lombok.AllArgsConstructor;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    /**
+     * ログイン処理
+     */
+    public User login(LoginForm loginForm) throws EntityNotFoundException {
+        final LoginInput loginInput = loginForm.getUser();
+        // メールアドレスとパスワードが一致するユーザーを取得
+        final User user = userRepository.findByEmailAndPassword(loginInput.getEmail(), loginInput.getPassword())
+                .orElseThrow(() -> new EntityNotFoundException("メールアドレスかパスワードが間違っています"));
+        return user;
+    }
 
     /**
      * Userを追加する
