6179df8836e742572fd3f4e125c777dbc7ee1d89
jfj2lk <parasol.sut.crt@gmail.com>
2025-04-08 07:01:18 +0900

    🛠️ メソッドの作成 ・ログイン処理に対応するメソッド作成。

--- spring/src/main/java/app/controller/UserController.java
+++ spring/src/main/java/app/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.form.user.add.AddUserForm;
+import app.form.user.login.LoginForm;
 import app.model.User;
 import app.service.UserService;
 import lombok.AllArgsConstructor;
@@ -26,9 +27,18 @@ public class UserController {
      * ユーザーを追加する
      */
     @PostMapping("/users")
-    public Object signup(@Validated @RequestBody AddUserForm addUserForm) {
+    public ResponseEntity<Map<String, Object>> signup(@Validated @RequestBody AddUserForm addUserForm) {
         final User addedUser = userService.addUser(addUserForm);
         return ResponseEntity.ok().body(Map.of("data", addedUser));
+    }
+
+    /**
+     * ログインする
+     */
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, Object>> login(@RequestBody LoginForm loginForm) {
+        final User loginUser = userService.login(loginForm);
+        return ResponseEntity.ok().body(Map.of("data", loginUser));
     }
 
 }
