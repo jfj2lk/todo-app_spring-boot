e958edc1319a713beeb23e539beefc704f502be1
jfj2lk <parasol.sut.crt@gmail.com>
2025-04-25 10:38:11 +0900

    🛠️ ユーザーシーダーファイルのパスワードを暗号化して保存するよう修正

--- spring/src/main/java/app/seed/UserSeeder.java
+++ spring/src/main/java/app/seed/UserSeeder.java
@@ -1,5 +1,7 @@
 package app.seed;
 
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import app.model.User;
@@ -11,13 +13,14 @@ import lombok.AllArgsConstructor;
 public class UserSeeder {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
     /**
      * ユーザーの初期データをシードする
      */
     public void seedInitialUser() {
-        final User user1 = new User("a", "a@a", "a");
-        final User user2 = new User("b", "b@b", "b");
+        final User user1 = new User("a", "a@a", passwordEncoder.encode("a"));
+        final User user2 = new User("b", "b@b", passwordEncoder.encode("b"));
         userRepository.save(user1);
         userRepository.save(user2);
     }
