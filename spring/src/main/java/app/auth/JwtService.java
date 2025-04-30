9865f492f082f12106e6832f8a0fbf7a0934ea2e
jfj2lk <parasol.sut.crt@gmail.com>
2025-04-30 11:00:32 +0900

    🛠️ 署名に使用する鍵をopensslで作成したものを使用するように変更

--- spring/src/main/java/app/auth/JwtService.java
+++ spring/src/main/java/app/auth/JwtService.java
@@ -1,34 +1,94 @@
 package app.auth;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.util.Date;
-
+import javax.crypto.SecretKey;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import app.model.User;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 @Service
 public class JwtService {
-    // 秘密鍵
-    // TODO: opensslで作成した秘密鍵に変える。
-    private final String SECRET_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9supersecretrandomstringforjwt";
-    // 署名に使用するキー
-    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
-    // 有効期限
-    private final long expirationDate = 1000 * 60 * 60;
+    /**
+     * フィールド
+     */
+    // 署名鍵のパス
+    private String signInKeyPath = "secret/hs256.key";
+    // HMAC用の署名鍵
+    private SecretKey hmacSignInKey;
+    // JWTの有効期限
+    private long expirationDate;
 
-    // JWTトークンの生成
+    /**
+     * コンストラクタ
+     */
+    public JwtService() {
+        this.hmacSignInKey = createHmacSignInKey();
+        this.expirationDate = createExpirationDate();
+    }
+
+    /**
+     * JWTの生成。
+     */
     // TODO:UserDetail実装に変える。
     public String generateToken(User user) {
-        return Jwts.builder()
-                .subject(String.valueOf(user
-                        .getId()))
+        return Jwts
+                .builder()
+                .subject(String.valueOf(user.getId()))
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + expirationDate))
-                .signWith(key)
+                .expiration(new Date(System.currentTimeMillis() + this.expirationDate))
+                .signWith(this.hmacSignInKey)
                 .compact();
+    }
+
+    /**
+     * 署名鍵ファイルの値をバイトとして取得。
+     */
+    public byte[] getSignInKeyBytes() {
+        try {
+            // ファイルからBASE64エンコードされた、秘密鍵の値を取得
+            ClassPathResource resource = new ClassPathResource(signInKeyPath);
+            byte[] bytes = resource.getInputStream().readAllBytes();
+            String base64EncodedPrivateKey = new String(bytes, StandardCharsets.UTF_8).trim();
+            // 秘密鍵をデコードして取得したバイト値を返す
+            return Decoders.BASE64.decode(base64EncodedPrivateKey);
+        } catch (IOException e) {
+            throw new RuntimeException("署名鍵ファイルの読み込みに失敗しました。", e);
+        }
+    }
+
+    /**
+     * HMAC形式の署名鍵を作成する。
+     */
+    public SecretKey createHmacSignInKey() {
+        // 署名鍵のバイト値
+        byte[] signInKeyBytes = getSignInKeyBytes();
+        return Keys.hmacShaKeyFor(signInKeyBytes);
+    }
+
+    /**
+     * JWT有効期限の作成。有効期限をミリ秒で返す。
+     */
+    public long createExpirationDate() {
+        // 1分の秒数
+        final long MINUTES_IN_SECONDS = 60;
+        // 1時間の分数
+        final long HOURS_IN_MINUTES = 60;
+
+        // 1秒のミリ秒数
+        final long SECONDS_IN_MILLIS = 1000;
+        // 1分のミリ秒
+        final long MINUTE_IN_MILLIS = SECONDS_IN_MILLIS * MINUTES_IN_SECONDS;
+        // 1時間のミリ秒
+        final long HOUR_IN_MILLIS = MINUTES_IN_SECONDS * HOURS_IN_MINUTES;
+
+        // 有効期限
+        final long EXPIRATION_DATE_IN_MILLIS = SECONDS_IN_MILLIS * 30;
+        return EXPIRATION_DATE_IN_MILLIS;
     }
 }
