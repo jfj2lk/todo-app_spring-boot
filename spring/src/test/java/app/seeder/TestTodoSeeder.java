b9b531e207d978ae3c1f514f03ff1c5e5986faf1
jfj2lk <parasol.sut.crt@gmail.com>
2025-05-03 16:56:49 +0900

    🛠️ テスト用のTodoシーダークラスの修正

--- spring/src/test/java/app/seeder/TestTodoSeeder.java
+++ spring/src/test/java/app/seeder/TestTodoSeeder.java
@@ -1,22 +1,25 @@
 package app.seeder;
 
+import java.util.List;
 import org.springframework.stereotype.Component;
 import app.model.Todo;
 import app.repository.TodoRepository;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @Component
+@Data
 @AllArgsConstructor
 public class TestTodoSeeder {
     private final TodoRepository todoRepository;
+    private final List<Todo> seedTodos = List.of(
+            new Todo("name1", "desc1"),
+            new Todo("name2", "desc2"));
 
     /**
      * テスト用のTodoの初期データをシードする
      */
     public void seedInitialTodo() {
-        final Todo todo1 = new Todo("todo1", "desc");
-        final Todo todo2 = new Todo("todo2", "desc");
-        todoRepository.save(todo1);
-        todoRepository.save(todo2);
+        todoRepository.saveAll(seedTodos);
     }
 }
