f41a2be56b7112fc00ff8e8a0e548a3e23a15bc4
jfj2lk <parasol.sut.crt@gmail.com>
2025-05-05 11:07:20 +0900

    🛠️ JWT作成処理の実装

--- spring/src/test/java/app/TestUtils.java
+++ spring/src/test/java/app/TestUtils.java
@@ -2,9 +2,26 @@ package app;
 
 import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import app.auth.JwtService;
+import app.model.User;
+import app.service.UserService;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class TestUtils {
+    private final JwtService jwtService;
+    private final UserService userService;
+
+    /**
+     * ユーザーID1のJWTを作成する。
+     */
+    public String createJwt() {
+        User user = new User("user1", "email1", userService.encryptPassword("password1"));
+        user.setId(1L);
+        return jwtService.generateJwt(user);
+    }
+
     /**
      * オブジェクトをJSON文字列に変換する。
      */
