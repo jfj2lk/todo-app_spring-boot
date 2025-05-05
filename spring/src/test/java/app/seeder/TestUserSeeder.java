7ac2b2ffd81ee4f239aee6d58f07c2a542a03276
jfj2lk <parasol.sut.crt@gmail.com>
2025-05-05 21:10:45 +0900

    ♻️ テスト用のユーザーシーダーの処理をリファクタリング

--- spring/src/test/java/app/seeder/TestUserSeeder.java
+++ spring/src/test/java/app/seeder/TestUserSeeder.java
@@ -1,26 +1,35 @@
 package app.seeder;
 
+import java.util.List;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import app.model.User;
 import app.repository.UserRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Component
+@Data
 @RequiredArgsConstructor
 public class TestUserSeeder {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder =
             PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    // テスト時に作成するUserの情報
+    private final List<User> seedUsers = List.of(
+            new User("a", "a@a", "a"),
+            new User("b", "b@b", "b"));
 
     /**
      * テスト用のユーザーの初期データをシードする
      */
     public void seedInitialUser() {
-        final User user1 = new User("a", "a@a", passwordEncoder.encode("a"));
-        final User user2 = new User("b", "b@b", passwordEncoder.encode("b"));
-        userRepository.save(user1);
-        userRepository.save(user2);
+        // 元のオブジェクトの情報が変更されないように、保存用のUser情報を作成
+        List<User> saveSeedUsers = seedUsers.stream()
+                .map(seedUser -> new User(seedUser.getName(), seedUser.getEmail(),
+                        passwordEncoder.encode(seedUser.getPassword())))
+                .toList();
+        userRepository.saveAll(saveSeedUsers);
     }
 }
