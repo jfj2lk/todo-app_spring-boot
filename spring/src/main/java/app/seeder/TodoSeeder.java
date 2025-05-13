9979bfd85e97871659c82108369cf2200c8794d2
jfj2lk <parasol.sut.crt@gmail.com>
2025-05-13 21:39:18 +0900

    ♻️ 期限日時カラムの追加に伴い、Todoシーダーの内容を修正。

--- spring/src/main/java/app/seeder/TodoSeeder.java
+++ spring/src/main/java/app/seeder/TodoSeeder.java
@@ -1,6 +1,7 @@
 package app.seeder;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -19,12 +20,13 @@ public class TodoSeeder {
      * Todoの初期データをシードする
      */
     public void seedInitialTodo() {
-        LocalDateTime now = LocalDateTime.now();
+        LocalDate nowDate = LocalDate.now();
+        LocalTime nowTime = LocalTime.now();
         List<Todo> saveTodos = List.of(
-                new Todo(1L, true, "todo1", "desc", 1, now),
-                new Todo(1L, false, "todo2", "desc", 2, now),
-                new Todo(2L, true, "todo3", "desc", 3, now),
-                new Todo(2L, false, "todo4", "desc", 4, now));
+                new Todo(1L, true, "todo1", "desc", 1, nowDate, nowTime),
+                new Todo(1L, false, "todo2", "desc", 2, nowDate, nowTime),
+                new Todo(2L, true, "todo3", "desc", 3, nowDate, nowTime),
+                new Todo(2L, false, "todo4", "desc", 4, nowDate, nowTime));
         todoRepository.saveAll(saveTodos);
     }
 }
--- spring/src/test/java/app/seeder/TestTodoSeeder.java
+++ spring/src/test/java/app/seeder/TestTodoSeeder.java
@@ -1,6 +1,7 @@
 package app.seeder;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import org.springframework.stereotype.Component;
 import app.model.Todo;
@@ -12,25 +13,27 @@ import lombok.RequiredArgsConstructor;
 @Data
 @RequiredArgsConstructor
 public class TestTodoSeeder {
-    private final TodoRepository todoRepository;
-    LocalDateTime now = LocalDateTime.now();
-    // テスト時に作成するTodoの情報
-    private final List<Todo> seedTodos = List.of(
-            new Todo(1L, 1L, true, "name1", "desc1", 1, now, null, null),
-            new Todo(2L, 1L, false, "name2", "desc2", 2, now, null, null),
-            new Todo(3L, 2L, true, "name3", "desc3", 3, now, null, null),
-            new Todo(4L, 2L, false, "name4", "desc4", 4, now, null, null));
+	private final TodoRepository todoRepository;
+	LocalDate nowDate = LocalDate.now();
+	LocalTime nowTime = LocalTime.now();
+	// テスト時に作成するTodoの情報
+	private final List<Todo> seedTodos = List.of(
+			new Todo(1L, 1L, true, "name1", "desc1", 1, nowDate, nowTime, null, null),
+			new Todo(2L, 1L, false, "name2", "desc2", 2, nowDate, nowTime, null, null),
+			new Todo(3L, 2L, true, "name3", "desc3", 3, nowDate, nowTime, null, null),
+			new Todo(4L, 2L, false, "name4", "desc4", 4, nowDate, nowTime, null, null));
 
-    /**
-     * テスト用のTodoの初期データを作成する
-     */
-    public void seedInitialTodo() {
-        // 元のオブジェクトの値が変更されないように、保存用のリストを作成する
-        List<Todo> saveTodos = seedTodos
-                .stream()
-                .map(seedTodo -> new Todo(seedTodo.getUserId(), seedTodo.getIsCompleted(),
-                        seedTodo.getName(), seedTodo.getDesc(), seedTodo.getPriority(), seedTodo.getDueDateTime()))
-                .toList();
-        todoRepository.saveAll(saveTodos);
-    }
+	/**
+	 * テスト用のTodoの初期データを作成する
+	 */
+	public void seedInitialTodo() {
+		// 元のオブジェクトの値が変更されないように、保存用のリストを作成する
+		List<Todo> saveTodos = seedTodos
+				.stream()
+				.map(seedTodo -> new Todo(seedTodo.getUserId(), seedTodo.getIsCompleted(),
+						seedTodo.getName(), seedTodo.getDesc(), seedTodo.getPriority(),
+						seedTodo.getDueDate(), seedTodo.getDueTime()))
+				.toList();
+		todoRepository.saveAll(saveTodos);
+	}
 }
