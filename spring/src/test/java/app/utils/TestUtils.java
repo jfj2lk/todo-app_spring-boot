0edaedf8836db21c95f179ab144585e59b90aebc
jfj2lk <parasol.sut.crt@gmail.com>
2025-05-20 16:23:53 +0900

    ♻️ パスワード関連の処理を別クラスのメソッドで行うよう修正。

--- spring/src/test/java/app/utils/TestUtils.java
+++ spring/src/test/java/app/utils/TestUtils.java
@@ -2,26 +2,24 @@ package app.utils;
 
 import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import app.auth.JwtService;
 import app.model.User;
-import app.service.AuthService;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class TestUtils {
     private final JwtService jwtService;
-    private final AuthService userService;
+    private final PasswordUtils passwordUtils;
     private static final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
     /**
      * 指定されたユーザーIDのJWTを作成する。
      */
     public String createJwt(Long userId) {
-        User user = new User("user1", "email1", userService.encryptPassword("password1"));
+        User user = new User("user1", "email1", passwordUtils.encode("password1"));
         user.setId(userId);
         return jwtService.generateJwt(user);
     }
