ee3c7bb8f0401274c0758303ad2743b8945b35a7
jfj2lk <parasol.sut.crt@gmail.com>
2025-05-20 16:21:22 +0900

    ♻️ パスワードに関する処理を別クラスの処理で行うように修正。

--- spring/src/main/java/app/service/AuthService.java
+++ spring/src/main/java/app/service/AuthService.java
@@ -1,14 +1,12 @@
 package app.service;
 
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import app.form.user.LoginForm;
 import app.form.user.SignUpForm;
 import app.model.User;
 import app.repository.UserRepository;
-
+import app.utils.PasswordUtils;
 import lombok.AllArgsConstructor;
 
 @Service
@@ -16,8 +14,7 @@ import lombok.AllArgsConstructor;
 public class AuthService {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder =
-            PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    private final PasswordUtils passwordUtils;
 
     /**
      * Userを追加する
@@ -25,8 +22,7 @@ public class AuthService {
     @Transactional
     public User signup(SignUpForm addUserForm) throws RuntimeException {
         // 同じメールアドレスのUserが存在する場合は例外を投げる
-        boolean existEmail =
-                userRepository.findByEmail(addUserForm.getEmail()).isPresent();
+        boolean existEmail = userRepository.findByEmail(addUserForm.getEmail()).isPresent();
         if (existEmail) {
             throw new RuntimeException("既に存在するメールアドレスです");
         }
@@ -35,7 +31,7 @@ public class AuthService {
         User addUser = new User(addUserForm);
         // パスワード暗号化
         String rawPassword = addUser.getPassword();
-        String encodedPassword = encryptPassword(rawPassword);
+        String encodedPassword = passwordUtils.encode(rawPassword);
         addUser.setPassword(encodedPassword);
         // Userを追加し、結果を返す
         return userRepository.save(addUser);
@@ -49,18 +45,10 @@ public class AuthService {
         User user = userRepository.findByEmail(loginForm.getEmail())
                 .orElseThrow(() -> new RuntimeException("メールアドレスかパスワードが間違っています。"));
         // パスワードが一致するかチェック
-        boolean isPasswordMatch =
-                passwordEncoder.matches(loginForm.getPassword(), user.getPassword());
+        boolean isPasswordMatch = passwordUtils.matches(loginForm.getPassword(), user.getPassword());
         if (!isPasswordMatch) {
             throw new RuntimeException("メールアドレスかパスワードが間違っています。");
         }
         return user;
-    }
-
-    /**
-     * パスワードを暗号化する（Bcrypt）。
-     */
-    public String encryptPassword(String rawPassword) {
-        return passwordEncoder.encode(rawPassword);
     }
 }
