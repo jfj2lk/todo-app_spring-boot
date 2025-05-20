ff1fc2b94224d4b922ba66805bc15b0ed2ecbc9f
jfj2lk <parasol.sut.crt@gmail.com>
2025-05-20 12:07:53 +0900

    🐛 使用していないクラスで、依存関係のエラーが発生していたので、処理を全てコメントアウトするよう修正。

--- spring/src/main/java/app/archive/annotation/CheckTodoExistsValidator.java
+++ spring/src/main/java/app/archive/annotation/CheckTodoExistsValidator.java
@@ -9,20 +9,21 @@ import lombok.RequiredArgsConstructor;
 
 // @Component
 // @RequiredArgsConstructor
-public class CheckTodoExistsValidator implements ConstraintValidator<CheckTodoExists, Long> {
+// public class CheckTodoExistsValidator implements
+// ConstraintValidator<CheckTodoExists, Long> {
 
-    private final TodoRepository todoRepository;
+// private final TodoRepository todoRepository;
 
-    @Override
-    public boolean isValid(Long id, ConstraintValidatorContext context) {
-        if (id == null || !todoRepository.existsById(id)) {
-            // デフォルトのメッセージを無効化
-            context.disableDefaultConstraintViolation();
-            // カスタムメッセージを設定
-            context.buildConstraintViolationWithTemplate("id=" + id + " のTodoが見つかりません")
-                    .addConstraintViolation();
-            return false;
-        }
-        return true;
-    }
-}
+// @Override
+// public boolean isValid(Long id, ConstraintValidatorContext context) {
+// if (id == null || !todoRepository.existsById(id)) {
+// // デフォルトのメッセージを無効化
+// context.disableDefaultConstraintViolation();
+// // カスタムメッセージを設定
+// context.buildConstraintViolationWithTemplate("id=" + id + " のTodoが見つかりません")
+// .addConstraintViolation();
+// return false;
+// }
+// return true;
+// }
+// }
