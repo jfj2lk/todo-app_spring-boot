09dc20ea82959d9b7c019bf587501b1a94062c75
jfj2lk <parasol.sut.crt@gmail.com>
2025-05-30 16:21:03 +0900

    🎨 新規登録フォーム作成。

--- vite/src/pages/auth/Signup.tsx
+++ vite/src/pages/auth/Signup.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { User } from "@/types/user";
 import { apiRequest } from "@/utils/api";
 import { useState } from "react";
@@ -14,7 +17,7 @@ const Signup = () => {
   const handleSignup = async (
     name: string,
     email: string,
-    password: string
+    password: string,
   ) => {
     const json = await apiRequest<User>("/api/auth/signup", "POST", {
       name,
@@ -28,51 +31,46 @@ const Signup = () => {
   };
 
   return (
-    <div>
+    <div className="flex h-lvh items-center justify-center bg-gray-100">
       <form
         onSubmit={(e) => {
           e.preventDefault();
           handleSignup(name, email, password);
         }}
+        className="flex w-[25%] flex-col items-center gap-4 rounded-md bg-white px-5 py-10"
       >
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "100px 250px",
-            gap: "5px",
-          }}
-        >
-          {/* 名前入力欄 */}
-          <label htmlFor="name">名前:</label>
-          <input
-            id="name"
-            type="text"
-            name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+        <h2 className="text-lg font-bold">新規登録</h2>
+        {/* 名前入力欄 */}
+        <Input
+          id="name"
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="名前"
+        />
 
-          {/* メールアドレス入力欄 */}
-          <label htmlFor="email">メールアドレス:</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+        {/* メールアドレス入力欄 */}
+        <Input
+          id="email"
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="メールアドレス"
+        />
 
-          {/* パスワード入力欄 */}
-          <label htmlFor="password">パスワード:</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button>新規登録</button>
+        {/* パスワード入力欄 */}
+        <Input
+          id="password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="パスワード"
+        />
+
+        <Button>新規登録</Button>
       </form>
     </div>
   );
