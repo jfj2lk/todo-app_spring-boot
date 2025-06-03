323f7cad93520ca47112a0e517ececd3ed40e796
jfj2lk <parasol.sut.crt@gmail.com>
2025-06-03 16:40:12 +0900

    🛠️ フォームの値でLabelオブジェクトの値を変更するメソッドの作成。

--- spring/src/main/java/app/model/Label.java
+++ spring/src/main/java/app/model/Label.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.Id;
 
+import app.form.label.AddLabelForm;
+import app.form.label.UpdateLabelForm;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,6 +23,26 @@ public class Label {
   public Label(String name) {
     LocalDateTime now = LocalDateTime.now();
     this.name = name;
+    this.createdAt = now;
+    this.updatedAt = now;
+  }
+
+  /**
+   * Labelフォームの値でLabelオブジェクトを作成する。
+   */
+  public Label(AddLabelForm addLabelForm) {
+    LocalDateTime now = LocalDateTime.now();
+    this.name = addLabelForm.getName();
+    this.createdAt = now;
+    this.updatedAt = now;
+  }
+
+  /**
+   * Labelフォームの値でLabelオブジェクトを作成する。
+   */
+  public void updateWithForm(UpdateLabelForm updateLabelForm) {
+    LocalDateTime now = LocalDateTime.now();
+    this.name = updateLabelForm.getName();
     this.createdAt = now;
     this.updatedAt = now;
   }
