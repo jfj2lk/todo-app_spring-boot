5416b9518b03b37d16bd6c4548e7e68178a73427
jfj2lk <parasol.sut.crt@gmail.com>
2025-06-03 14:11:12 +0900

    🛢️ TodoシーダーでTodoにラベルを紐づける処理を追加。

--- spring/src/main/java/app/seeder/TodoSeeder.java
+++ spring/src/main/java/app/seeder/TodoSeeder.java
@@ -6,7 +6,11 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
+import app.model.Label;
 import app.model.Todo;
+import app.model.TodoLabel;
+import app.repository.LabelRepository;
+import app.repository.TodoLabelRepository;
 import app.repository.TodoRepository;
 import lombok.AllArgsConstructor;
 
@@ -14,6 +18,8 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class TodoSeeder {
     private final TodoRepository todoRepository;
+    private final LabelRepository labelRepository;
+    private final TodoLabelRepository todoLabelRepository;
 
     /**
      * Todoの初期データをシードする
@@ -21,11 +27,22 @@ public class TodoSeeder {
     public void seedInitialTodo() {
         LocalDate nowDate = LocalDate.now();
         LocalTime nowTime = LocalTime.now();
-        List<Todo> saveTodos = List.of(
+        List<Todo> todos = List.of(
                 new Todo(1L, true, "todo1", "desc", 1, nowDate, nowTime),
                 new Todo(1L, false, "todo2", "desc", 2, nowDate, nowTime),
                 new Todo(2L, true, "todo3", "desc", 3, nowDate, nowTime),
                 new Todo(2L, false, "todo4", "desc", 4, nowDate, nowTime));
-        todoRepository.saveAll(saveTodos);
+        todoRepository.saveAll(todos);
+
+        List<Label> labels = List.of(
+                new Label("家事"),
+                new Label("健康"));
+        labelRepository.saveAll(labels);
+
+        List<TodoLabel> todoLabels = List.of(
+                new TodoLabel(1L, 1L),
+                new TodoLabel(2L, 1L),
+                new TodoLabel(1L, 2L));
+        todoLabelRepository.saveAll(todoLabels);
     }
 }
