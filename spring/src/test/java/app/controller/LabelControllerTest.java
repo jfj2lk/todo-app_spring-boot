ebf87de7023c404fd48b2ba959f896e6f99d826f
jfj2lk <parasol.sut.crt@gmail.com>
2025-06-03 17:04:18 +0900

    ✅ Label取得と追加用のテストの作成。

---
+++ spring/src/test/java/app/controller/LabelControllerTest.java
@@ -0,0 +1,89 @@
+package app.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import app.form.label.AddLabelForm;
+import app.model.Label;
+import app.repository.LabelRepository;
+import app.utils.TestUtils;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class LabelControllerTest {
+  @Autowired
+  private MockMvc mockMvc;
+  @Autowired
+  private LabelRepository labelRepository;
+  @Autowired
+  private TestUtils testUtils;
+
+  private String jwt;
+
+  @BeforeEach
+  void setUpAll() {
+    jwt = this.testUtils.createJwt(1L);
+
+    List<Label> labels = List.of(new Label("label1"), new Label("label2"));
+    labelRepository.saveAll(labels);
+  }
+
+  @Test
+  void 全てのLabelを取得() throws Exception {
+    mockMvc.perform(get("/api/labels")
+        .header("Authorization", "Bearer " + this.jwt))
+        .andExpectAll(
+            status().isOk(),
+            content().contentType(MediaType.APPLICATION_JSON),
+            jsonPath("$.data.length()").value(2),
+            jsonPath("$.data.[0].id").value(1),
+            jsonPath("$.data.[0].name").value("label1"),
+            jsonPath("$.data.[0].createdAt").exists(),
+            jsonPath("$.data.[0].updatedAt").exists());
+  }
+
+  @Test
+  void Labelを追加() throws Exception {
+    // 追加されたLabelの期待するID
+    long expectedAddLabelId = this.labelRepository.count() + 1;
+    // Label追加後の期待するLabelの数
+    long expectedTotalLabelCount = this.labelRepository.count() + 1;
+
+    // Label追加用のフォームを作成
+    AddLabelForm addLabelForm = new AddLabelForm("addLabel");
+    String addLabelFormJson = this.testUtils.toJson(addLabelForm);
+
+    mockMvc.perform(post("/api/labels")
+        .header("Authorization", "Bearer " + this.jwt)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(addLabelFormJson))
+        .andExpectAll(
+            status().isOk(),
+            content().contentType(MediaType.APPLICATION_JSON),
+            jsonPath("$.data.id").value(expectedAddLabelId),
+            jsonPath("$.data.name").value(addLabelForm.getName()),
+            jsonPath("$.data.createdAt").exists(),
+            jsonPath("$.data.updatedAt").exists());
+
+    // レコードが1件分増えていることを確認
+    assertEquals(expectedTotalLabelCount, this.labelRepository.count());
+  }
+}
