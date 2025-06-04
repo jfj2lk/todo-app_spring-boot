77c1ddd86782d74d59b34d79406447f4be8c7186
jfj2lk <parasol.sut.crt@gmail.com>
2025-06-04 13:21:03 +0900

    🛠️ 指定されたTodoIdに紐づく全てのレコードを返す処理を実装。

--- spring/src/main/java/app/repository/TodoLabelRepository.java
+++ spring/src/main/java/app/repository/TodoLabelRepository.java
@@ -1,9 +1,11 @@
 package app.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import app.model.TodoLabel;
 
 public interface TodoLabelRepository extends CrudRepository<TodoLabel, Long> {
-
+    List<TodoLabel> findAllByTodoId(Long todoId);
 }
