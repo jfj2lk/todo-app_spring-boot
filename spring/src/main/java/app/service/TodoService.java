721e33d944d0dcd68ed25c747295793a6dc5296f
jfj2lk <parasol.sut.crt@gmail.com>
2025-06-05 17:44:26 +0900

    ♻️ TodoLabelのスキーマ変更に伴い、Todo追加・更新処理の内容を修正。 ・中間テーブルにレコードを保存する処理が不要になった為削除。 ・フォームのLabelIdsに対応する全てのレコードが存在するか確認する処理を最初に行う要修正。

--- spring/src/main/java/app/service/TodoService.java
+++ spring/src/main/java/app/service/TodoService.java
@@ -4,14 +4,11 @@ import app.form.todo.AddTodoForm;
 import app.form.todo.UpdateTodoForm;
 import app.model.Label;
 import app.model.Todo;
-import app.model.TodoLabel;
 import app.repository.LabelRepository;
-import app.repository.TodoLabelRepository;
 import app.repository.TodoRepository;
 import app.utils.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
 import java.util.Set;
 import java.util.stream.StreamSupport;
 
@@ -25,7 +22,6 @@ public class TodoService {
     private final TodoRepository todoRepository;
     private final SecurityUtils securityUtils;
     private final LabelRepository labelRepository;
-    private final TodoLabelRepository todoLabelRepository;
 
     /**
      * 全てのTodoをDBから取得する。
@@ -41,28 +37,29 @@ public class TodoService {
      * Todoを追加する。
      */
     public Todo addTodo(AddTodoForm addTodoForm) {
+        // 指定されたLabelIdsに対応するレコードが全て存在しない場合は例外を投げる
+        if (!isAllLabelsExist(addTodoForm.getLabelIds())) {
+            throw new RuntimeException("指定されたラベルが見つかりません。");
+        }
+
         // ログイン中のユーザーIDを取得
         Long currentUserId = securityUtils.getCurrentUserId();
         // フォームの値でTodoオブジェクトを作成する
         Todo addTodo = new Todo(addTodoForm, currentUserId);
-        // Todo追加
-        Todo addedTodo = todoRepository.save(addTodo);
 
-        // フォームにlabelIdが含まれている場合は、中間テーブルに保存する
-        Set<Long> labelIds = addTodoForm.getLabelIds();
-        if (labelIds != null) {
-            saveTodoLabelRelation(addedTodo.getId(), labelIds);
-        }
-
-        // 追加されたTodo情報を返す
-        return addedTodo;
+        // Todoを追加し、追加後の値を返す
+        return todoRepository.save(addTodo);
     }
 
     /**
      * Todoを更新する。
      */
-    public Todo updateTodo(Long todoId, UpdateTodoForm updateTodoForm)
-            throws RuntimeException {
+    public Todo updateTodo(Long todoId, UpdateTodoForm updateTodoForm) {
+        // 指定されたLabelIdsに対応するレコードが全て存在しない場合は例外を投げる
+        if (!isAllLabelsExist(updateTodoForm.getLabelIds())) {
+            throw new RuntimeException("指定されたラベルが見つかりません。");
+        }
+
         // ログイン中のユーザーIDを取得
         Long currentUserId = securityUtils.getCurrentUserId();
         // 更新対象のTodoをDBから取得
@@ -70,16 +67,9 @@ public class TodoService {
                 .orElseThrow(() -> new RuntimeException("更新対象のTodoが見つかりませんでした。"));
         // フォームの値でTodoオブジェクトを更新する
         updateTodo.updateWithForm(updateTodoForm);
-        // Todo更新
-        Todo updatedTodo = todoRepository.save(updateTodo);
 
-        // フォームにlabelIdが含まれている場合は、中間テーブルに保存する
-        Set<Long> labelIds = updateTodoForm.getLabelIds();
-        if (labelIds != null) {
-            saveTodoLabelRelation(updateTodo.getId(), updateTodoForm.getLabelIds());
-        }
-
-        return updatedTodo;
+        // Todoを更新し、更新後の値を返す
+        return todoRepository.save(updateTodo);
     }
 
     /**
@@ -112,49 +102,21 @@ public class TodoService {
         return todoRepository.save(updateTodo);
     }
 
-    /**
-     * 指定したTodoとLabelの関連を保存する
+    /*
+     * 指定されたLabelがDBに全て存在するか確認する
      */
-    public void saveTodoLabelRelation(Long todoId, Set<Long> newLabelIds) {
-        // 指定されたLabelIdsの内、実際にDBに存在するLabelのIDを取得
-        List<Long> existingLabels = StreamSupport
-                .stream(labelRepository.findAllById(newLabelIds).spliterator(), false)
-                .map(Label::getId)
-                .toList();
-        // 指定されたLabelIdsのLabelが全て存在しなかった場合
-        if (!existingLabels.containsAll(newLabelIds)) {
-            List<Long> notFoundIds = newLabelIds.stream()
-                    .filter(newLabelId -> !existingLabels.contains(newLabelId))
-                    .toList();
-            throw new RuntimeException("指定されたラベルが見つかりません。" + notFoundIds);
+    public boolean isAllLabelsExist(Set<Long> labelIds) {
+        // nullや中身が空の場合は、全てのLabelが存在すると判定する
+        if (labelIds == null || labelIds.isEmpty()) {
+            return true;
         }
 
-        // 指定されたTodoIdに紐づく中間レコードのIDを全て取得
-        List<TodoLabel> todoLabelsForTodoId = todoLabelRepository.findAllByTodoId(todoId);
+        // IDが一致するレコードを全て取得する
+        Iterable<Label> existingLabels = labelRepository.findAllById(labelIds);
+        // IDが一致したレコードの数を取得する
+        long existingLabelsCount = StreamSupport.stream(existingLabels.spliterator(), false).count();
 
-        // 指定されたTodoIdに紐づく中間レコードの全てのLabelのIDを取得
-        List<Long> currentLabelIds = todoLabelsForTodoId.stream()
-                .map(TodoLabel::getLabelId).toList();
-
-        // 新たに追加する中間レコード
-        List<TodoLabel> todoLabelsForAdd = newLabelIds.stream()
-                // 新しいラベルの内、現在のラベルに含まれていないものを抽出する
-                .filter(newLabelId -> !currentLabelIds.contains(newLabelId))
-                .map(addLabelId -> new TodoLabel(todoId, addLabelId))
-                .toList();
-
-        // 削除する中間レコードのIDのリストを作成
-        List<TodoLabel> todoLabelsForDelete = todoLabelsForTodoId.stream()
-                // 現在のラベルの内、新しいラベルに含まれていないものを抽出する
-                .filter(todoLabelForTodoId -> !newLabelIds.contains(todoLabelForTodoId.getLabelId()))
-                .toList();
-
-        // 保存と削除を実行
-        if (!todoLabelsForAdd.isEmpty()) {
-            todoLabelRepository.saveAll(todoLabelsForAdd);
-        }
-        if (!todoLabelsForDelete.isEmpty()) {
-            todoLabelRepository.deleteAll(todoLabelsForDelete);
-        }
+        // 指定されたIDの数と実際に存在するレコードの数が一致しているか判定する
+        return existingLabelsCount == labelIds.size();
     }
 }
