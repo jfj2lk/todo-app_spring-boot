ea50f716b945474d46f4eb4a2558796168c1cbe8
jfj2lk <parasol.sut.crt@gmail.com>
2025-06-07 18:04:17 +0900

    🛠️ シーダーの内容にプロジェクトを作成する内容を追加。

--- spring/src/main/java/app/seeder/TodoSeeder.java
+++ spring/src/main/java/app/seeder/TodoSeeder.java
@@ -8,41 +8,50 @@ import java.util.Set;
 import org.springframework.stereotype.Component;
 
 import app.model.Label;
+import app.model.Project;
 import app.model.Todo;
 import app.model.TodoLabel;
 import app.repository.LabelRepository;
+import app.repository.ProjectRepository;
 import app.repository.TodoRepository;
 import lombok.AllArgsConstructor;
 
 @Component
 @AllArgsConstructor
 public class TodoSeeder {
-        private final TodoRepository todoRepository;
-        private final LabelRepository labelRepository;
+    private final TodoRepository todoRepository;
+    private final LabelRepository labelRepository;
+    private final ProjectRepository projectRepository;
 
-        /**
-         * Todoの初期データをシードする
-         */
-        public void seedInitialTodo() {
-                LocalDate nowDate = LocalDate.now();
-                LocalTime nowTime = LocalTime.now();
+    /**
+     * Todoの初期データをシードする
+     */
+    public void seedInitialTodo() {
+        LocalDate nowDate = LocalDate.now();
+        LocalTime nowTime = LocalTime.now();
 
-                // Labelの初期データ作成
-                List<Label> labels = List.of(
-                                new Label("家事"),
-                                new Label("健康"));
-                labelRepository.saveAll(labels);
+        // Projectの初期データ作成
+        List<Project> lists = List.of(
+                new Project("プライベート"),
+                new Project("仕事"));
+        projectRepository.saveAll(lists);
 
-                // Todoの初期データ作成
-                List<Todo> todos = List.of(
-                                new Todo(1L, true, "todo1", "desc1", 1, nowDate, nowTime,
-                                                Set.of(new TodoLabel(1L))),
-                                new Todo(1L, false, "todo2", "desc2", 2, nowDate, nowTime,
-                                                Set.of(new TodoLabel(1L), new TodoLabel(2L))),
-                                new Todo(2L, true, "todo3", "desc3", 3, nowDate, nowTime,
-                                                Set.of(new TodoLabel(1L))),
-                                new Todo(2L, false, "todo4", "desc4", 4, nowDate, nowTime,
-                                                Set.of(new TodoLabel(1L), new TodoLabel(2L))));
-                todoRepository.saveAll(todos);
-        }
+        // Labelの初期データ作成
+        List<Label> labels = List.of(
+                new Label("家事"),
+                new Label("健康"));
+        labelRepository.saveAll(labels);
+
+        // Todoの初期データ作成
+        List<Todo> todos = List.of(
+                new Todo(1L, 1L, true, "todo1", "desc1", 1, nowDate, nowTime,
+                        Set.of(new TodoLabel(1L))),
+                new Todo(1L, 2L, false, "todo2", "desc2", 2, nowDate, nowTime,
+                        Set.of(new TodoLabel(1L), new TodoLabel(2L))),
+                new Todo(2L, 1L, true, "todo3", "desc3", 3, nowDate, nowTime,
+                        Set.of(new TodoLabel(1L))),
+                new Todo(2L, 2L, false, "todo4", "desc4", 4, nowDate, nowTime,
+                        Set.of(new TodoLabel(1L), new TodoLabel(2L))));
+        todoRepository.saveAll(todos);
+    }
 }
