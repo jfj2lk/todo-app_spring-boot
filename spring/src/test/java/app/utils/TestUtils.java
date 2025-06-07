e02e2f3dc61907f4436848ddda508d539bbb7686
jfj2lk <parasol.sut.crt@gmail.com>
2025-06-07 10:24:32 +0900

    🛠️ テスト用のユーティリティクラスにJSON変換用のメソッド追加。

--- spring/src/test/java/app/utils/TestUtils.java
+++ spring/src/test/java/app/utils/TestUtils.java
@@ -1,6 +1,13 @@
 package app.utils;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
@@ -10,23 +17,76 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class TestUtils {
-    private final JwtUtils jwtService;
+    private final JwtUtils jwtUtils;
     private final PasswordUtils passwordUtils;
-    private static final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    private ObjectMapper mapper = createMapper();
 
     /**
-     * 指定されたユーザーIDのJWTを作成する。
+     * ObjectMapperを作成する。
+     */
+    public ObjectMapper createMapper() {
+        return new ObjectMapper().registerModule(new JavaTimeModule());
+    }
+
+    /**
+     * 指定されたIDのユーザーのJWTを作成する。
      */
     public String createJwt(Long userId) {
-        User user = new User("user1", "email1", passwordUtils.encode("password1"));
-        user.setId(userId);
-        return jwtService.generateJwt(user);
+        LocalDateTime now = LocalDateTime.now();
+        User user = new User(userId, "user1", "email1", passwordUtils.encode("password1"), now, now);
+        return jwtUtils.generateJwt(user);
     }
 
     /**
      * オブジェクトをJSON文字列に変換する。
      */
     public String toJson(Object object) throws Exception {
-        return objectMapper.writeValueAsString(object);
+        return mapper.writeValueAsString(object);
+    }
+
+    /**
+     * JSON文字列を指定のオブジェクトに変換する。
+     */
+    public <T> T fromJson(String jsonString, Class<T> classType) throws Exception {
+        return mapper.readValue(jsonString, classType);
+    }
+
+    /**
+     * JSON文字列から、指定されたフィールドの値を抽出する。
+     */
+    public JsonNode extractFieldFromJson(String jsonString, String fieldName) throws Exception {
+        return mapper.readTree(jsonString).get(fieldName);
+    }
+
+    /**
+     * JsonNodeを指定のオブジェクトに変換する。
+     */
+    public <T> T fromJsonNode(JsonNode jsonNode, Class<T> classType) throws Exception {
+        return mapper.treeToValue(jsonNode, classType);
+    }
+
+    /**
+     * JSON文字列から指定されたフィールドの値を抽出し、指定のオブジェクトに変換する。
+     */
+    public <T> T fieldFromJson(String jsonString, String fieldName, Class<T> classType) throws Exception {
+        JsonNode jsonNode = extractFieldFromJson(jsonString, fieldName);
+        return fromJsonNode(jsonNode, classType);
+    }
+
+    /**
+     * JSON文字列から指定されたフィールドの指定されたインデックスの値を抽出し、指定のオブジェクトに変換する。
+     */
+    public <T> T fieldIndexFromJson(String jsonString, String fieldName, int index, Class<T> classType)
+            throws Exception {
+        JsonNode jsonNode = extractFieldFromJson(jsonString, fieldName).get(index);
+        return fromJsonNode(jsonNode, classType);
+    }
+
+    /**
+     * IterableをListに変換する。
+     */
+    public <T> List<T> toList(Iterable<T> iterable) {
+        return StreamSupport.stream(iterable.spliterator(), false)
+                .collect(Collectors.toList());
     }
 }
