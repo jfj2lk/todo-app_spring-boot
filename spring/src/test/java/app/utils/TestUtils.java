8598d3f314cb11c4ee2e41c982ad8d8f589ce8b0
jfj2lk <parasol.sut.crt@gmail.com>
2025-06-12 18:01:41 +0900

    ♻️ TestUtilsのJson関連のメソッドのリファクタリング。

--- spring/src/test/java/app/utils/TestUtils.java
+++ spring/src/test/java/app/utils/TestUtils.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import app.model.User;
@@ -22,19 +23,19 @@ public class TestUtils {
     private ObjectMapper mapper = createMapper();
 
     /**
-     * ObjectMapperを作成する。
-     */
-    public ObjectMapper createMapper() {
-        return new ObjectMapper().registerModule(new JavaTimeModule());
-    }
-
-    /**
-     * 指定されたIDのユーザーのJWTを作成する。
+     * 指定されたユーザーIDのJWTを作成する。
      */
     public String createJwt(Long userId) {
         LocalDateTime now = LocalDateTime.now();
         User user = new User(userId, "user1", "email1", passwordUtils.encode("password1"), now, now);
         return jwtUtils.generateJwt(user);
+    }
+
+    /**
+     * ObjectMapperを作成する。
+     */
+    public ObjectMapper createMapper() {
+        return new ObjectMapper().registerModule(new JavaTimeModule());
     }
 
     /**
@@ -45,48 +46,30 @@ public class TestUtils {
     }
 
     /**
-     * JSON文字列を指定のオブジェクトに変換する。
+     * JSON文字列から指定されたフィールドの値を抽出し、指定のオブジェクトに変換する。
      */
-    public <T> T fromJson(String jsonString, Class<T> classType) throws Exception {
-        return mapper.readValue(jsonString, classType);
-    }
-
-    /**
-     * JSON文字列から、指定されたフィールドの値を抽出する。
-     */
-    public JsonNode extractFieldFromJson(String jsonString, String fieldName) throws Exception {
-        return mapper.readTree(jsonString).get(fieldName);
-    }
-
-    /**
-     * JsonNodeを指定のオブジェクトに変換する。
-     */
-    public <T> T fromJsonNode(JsonNode jsonNode, Class<T> classType) throws Exception {
+    public <T> T fromJsonField(String jsonString, String fieldName, Class<T> classType) throws Exception {
+        JsonNode jsonNode = mapper.readTree(jsonString).get(fieldName);
         return mapper.treeToValue(jsonNode, classType);
     }
 
     /**
-     * JSON文字列から指定されたフィールドの値を抽出し、指定のオブジェクトに変換する。
+     * JSON文字列から指定されたフィールドの値を抽出し、List型でラップした指定のオブジェクトに変換する。
      */
-    public <T> T fieldFromJson(String jsonString, String fieldName, Class<T> classType) throws Exception {
-        JsonNode jsonNode = extractFieldFromJson(jsonString, fieldName);
-        return fromJsonNode(jsonNode, classType);
-    }
-
-    /**
-     * JSON文字列から指定されたフィールドの指定されたインデックスの値を抽出し、指定のオブジェクトに変換する。
-     */
-    public <T> T fieldIndexFromJson(String jsonString, String fieldName, int index, Class<T> classType)
-            throws Exception {
-        JsonNode jsonNode = extractFieldFromJson(jsonString, fieldName).get(index);
-        return fromJsonNode(jsonNode, classType);
+    public <T> List<T> fromJsonFieldAsList(String jsonString, String fieldName, Class<T> classType) throws Exception {
+        JsonNode jsonNode = mapper.readTree(jsonString).get(fieldName);
+        CollectionType listType = mapper
+                .getTypeFactory()
+                .constructCollectionType(List.class, classType);
+        return mapper.convertValue(jsonNode, listType);
     }
 
     /**
      * IterableをListに変換する。
      */
     public <T> List<T> toList(Iterable<T> iterable) {
-        return StreamSupport.stream(iterable.spliterator(), false)
+        return StreamSupport.stream(
+                iterable.spliterator(), false)
                 .collect(Collectors.toList());
     }
 }
