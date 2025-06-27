87df17e71ed43f6e9c780185e906955d3c785b66
jfj2lk <parasol.sut.crt@gmail.com>
2025-06-27 11:01:30 +0900

    🐛 EntityActionButtonが正常に機能するように修正。

--- vite/src/components/entity/EntityIcon.tsx
+++ vite/src/components/entity/EntityIcon.tsx
@@ -2,11 +2,14 @@ import { Button } from "@/components/ui/button";
 import { ReactNode } from "react";
 
 const EntityIcon = (props: { children: ReactNode; className?: string }) => {
+  const { children, className, ...rest } = props;
+
   return (
     <Button
       className={`entity-manager_icon ${props.className}`}
       variant={"ghost"}
       size={"icon"}
+      {...rest}
     >
       {props.children}
     </Button>
