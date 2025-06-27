668324f91fc5538f9d19a0a2d04b39a3c08e0ee4
jfj2lk <parasol.sut.crt@gmail.com>
2025-06-27 15:01:58 +0900

    ♻️ Label関連のコンポーネントを削除し、LabelPageを新たに作成。ReduxストアをProviderでラップし、ルーティングを整理。

--- vite/src/index.tsx
+++ vite/src/index.tsx
@@ -8,36 +8,40 @@ import "@/utils/axios-default-config";
 import { MantineProvider } from "@mantine/core";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Signup from "./pages/auth/Signup";
-import Labels from "./pages/label/Labels";
+import LabelPage from "./pages/label/Labels";
 import ProjectPage from "./pages/project/ProjectPage";
 import Todos from "./pages/todo/Todos";
+import { store } from "./store";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="" element={<Layout />}>
-            {/* マーケティングページ */}
-            <Route path="" element={<MarketingLayout />}>
-              <Route path="/" element={<Home />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/login" element={<Login />} />
-            </Route>
-            {/* アプリページ */}
-            <Route path="" element={<ProtectedRoute />}>
-              <Route path="" element={<AppLayout />}>
-                <Route path="/projects/:id" element={<Todos />} />
-                <Route path="/labels" element={<Labels />} />
-                <Route path="/projects" element={<ProjectPage />} />
+    <Provider store={store}>
+      <MantineProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="" element={<Layout />}>
+              {/* マーケティングページ */}
+              <Route path="" element={<MarketingLayout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+              </Route>
+              {/* アプリページ */}
+              <Route path="" element={<ProtectedRoute />}>
+                <Route path="" element={<AppLayout />}>
+                  <Route path="/projects/:id" element={<Todos />} />
+                  <Route path="/labels" element={<LabelPage />} />
+                  <Route path="/projects" element={<ProjectPage />} />
+                </Route>
               </Route>
             </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </MantineProvider>
+          </Routes>
+        </BrowserRouter>
+      </MantineProvider>
+    </Provider>
   </StrictMode>,
 );
--- vite/src/pages/label/Labels.tsx
+++ vite/src/pages/label/Labels.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { EntityManager } from "@/components/entity/EntityManager";
-import { Button } from "@/components/ui/button";
 import { useAppSelector } from "@/store";
 import {
   createLabel,
@@ -10,130 +9,27 @@ import {
   labelSelectors,
   updateLabel,
 } from "@/store/labelStore";
-import {
-  defaultLabelFormValues,
-  labelFormSchema,
-  LabelType,
-} from "@/types/label";
-import { apiRequest } from "@/utils/api";
-import { Plus, Tag } from "lucide-react";
-import { useEffect, useState } from "react";
-import { LabelList } from "./LabelList";
-import { LabelModal } from "./LabelModal";
+import { defaultLabelFormValues, labelFormSchema } from "@/types/label";
+import { Tag } from "lucide-react";
 
-const Labels = () => {
+const LabelPage = () => {
   const labels = useAppSelector(labelSelectors.selectAll);
 
-  const [labelsState, setLabels] = useState<LabelType[]>([]);
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [editingLabel, setEditingLabel] = useState<LabelType | null>(null);
-
-  const onAddLabel = () => {
-    setEditingLabel(null);
-    setIsModalOpen(true);
-  };
-
-  const onEditLabel = (label: LabelType) => {
-    setEditingLabel(label);
-    setIsModalOpen(true);
-  };
-
-  const onCloseModal = () => {
-    setIsModalOpen(false);
-    setEditingLabel(null);
-  };
-
-  // Label追加
-  const handleAddLabel = async (addLabel: { name: string }) => {
-    const json = await apiRequest<LabelType>("/api/labels", "POST", addLabel);
-    setLabels([...labelsState, json.data]);
-
-    setIsModalOpen(false);
-    setEditingLabel(null);
-  };
-
-  // Label更新
-  const handleUpdateLabel = async (
-    updateLabelId: number,
-    updateLabel: { name: string },
-  ) => {
-    const json = await apiRequest<LabelType>(
-      `/api/labels/${updateLabelId}`,
-      "PATCH",
-      updateLabel,
-    );
-    setLabels(
-      labelsState.map((label) =>
-        label.id === updateLabelId ? json.data : label,
-      ),
-    );
-
-    setIsModalOpen(false);
-    setEditingLabel(null);
-  };
-
-  // Label削除
-  const handleDeleteLabel = async (deleteLabelId: number) => {
-    const json = await apiRequest<number>(
-      `/api/labels/${deleteLabelId}`,
-      "DELETE",
-    );
-    setLabels(labelsState.filter((label) => label.id !== json.data));
-  };
-
-  // 全てのLabelを取得し、stateを初期化する
-  useEffect(() => {
-    (async () => {
-      const json = await apiRequest<LabelType[]>("/api/labels");
-      setLabels(json.data);
-    })();
-  }, []);
-
   return (
-    <div className="container mx-auto max-w-4xl p-6">
-      <div className="space-y-6">
-        <EntityManager
-          entities={labels}
-          formSchema={labelFormSchema}
-          defaultFormValues={defaultLabelFormValues}
-          getAllEntities={getAllLabels}
-          createEntity={createLabel}
-          updateEntity={updateLabel}
-          deleteEntity={deleteLabel}
-          entityName="ラベル"
-          entityIcon={<Tag />}
-        />
-
-        {/*  */}
-        <div className="flex items-center justify-between">
-          <h1 className="text-2xl font-semibold">ラベル</h1>
-          <Button
-            variant="ghost"
-            size="icon"
-            onClick={onAddLabel}
-            className="text-muted-foreground hover:text-foreground h-8 w-8"
-          >
-            <Plus className="h-4 w-4" />
-          </Button>
-        </div>
-
-        <LabelList
-          labels={labelsState}
-          onEditLabel={onEditLabel}
-          onDeleteLabel={handleDeleteLabel}
-        />
-
-        <LabelModal
-          isOpen={isModalOpen}
-          onClose={onCloseModal}
-          handleAddLabel={handleAddLabel}
-          handleUpdateLabel={handleUpdateLabel}
-          editingLabel={editingLabel}
-        />
-      </div>
+    <div className="container mx-auto max-w-4xl space-y-6 p-6">
+      <EntityManager
+        entities={labels}
+        formSchema={labelFormSchema}
+        defaultFormValues={defaultLabelFormValues}
+        getAllEntities={getAllLabels}
+        createEntity={createLabel}
+        updateEntity={updateLabel}
+        deleteEntity={deleteLabel}
+        entityName="ラベル"
+        entityIcon={<Tag />}
+      />
     </div>
   );
 };
 
-export default Labels;
+export default LabelPage;
