425d27ae3a25ad16cb11b971a984015d1d205c37
jfj2lk <parasol.sut.crt@gmail.com>
2025-06-28 15:16:00 +0900

    ♻️ ルーティング設定の修正。 ・不要なルーティングを削除。

--- vite/src/index.tsx
+++ vite/src/index.tsx
@@ -1,6 +1,5 @@
 import AppLayout from "@/components/AppLayout";
 import MarketingLayout from "@/components/MarketingLayout";
-import ProtectedRoute from "@/components/ProtectedRoute";
 import "@/global.css";
 import Layout from "@/layout";
 import Home from "@/pages/home/Home";
@@ -12,8 +11,6 @@ import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Signup from "./pages/auth/Signup";
-import LabelPage from "./pages/label/Labels";
-import ProjectPage from "./pages/project/ProjectPage";
 import Todos from "./pages/todo/Todos";
 import { store } from "./store";
 
@@ -23,20 +20,17 @@ createRoot(document.getElementById("root")!).render(
       <MantineProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="" element={<Layout />}>
+            <Route element={<Layout />}>
               {/* マーケティングページ */}
-              <Route path="" element={<MarketingLayout />}>
-                <Route path="/" element={<Home />} />
+              <Route element={<MarketingLayout />}>
+                <Route index element={<Home />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
               </Route>
+
               {/* アプリページ */}
-              <Route path="" element={<ProtectedRoute />}>
-                <Route path="" element={<AppLayout />}>
-                  <Route path="/projects/:id" element={<Todos />} />
-                  <Route path="/labels" element={<LabelPage />} />
-                  <Route path="/projects" element={<ProjectPage />} />
-                </Route>
+              <Route element={<AppLayout />}>
+                <Route path="/projects/:id" element={<Todos />} />
               </Route>
             </Route>
           </Routes>
