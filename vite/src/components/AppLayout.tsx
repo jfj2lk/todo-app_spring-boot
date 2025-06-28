fdb22b25788a97bf98380bdc4945a5d84bb600df
jfj2lk <parasol.sut.crt@gmail.com>
2025-06-28 15:16:42 +0900

    ♻️ ProtectedRouteで行っていた処理をAppLayoutで行うように修正。

--- vite/src/components/AppLayout.tsx
+++ vite/src/components/AppLayout.tsx
@@ -1,8 +1,14 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import AppSidebar from "./AppSidebar";
 
 const AppLayout = () => {
+  const navigate = useNavigate();
+  // アクセストークンを持っていない場合はホーム画面に遷移させる
+  if (!localStorage.getItem("accessToken")) {
+    navigate("/");
+  }
+
   return (
     <SidebarProvider>
       <AppSidebar />
