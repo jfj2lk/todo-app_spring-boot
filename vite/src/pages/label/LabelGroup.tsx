b8ebee29181c85e588d31261e37d3240ecd39329
jfj2lk <parasol.sut.crt@gmail.com>
2025-06-28 11:54:03 +0900

    ♻️ LabelGroupコンポーネントをBaseSidebarGroupを元に作成するよう修正。

---
+++ vite/src/pages/label/LabelGroup.tsx
@@ -0,0 +1,29 @@
+import { BaseSidebarGroup } from "@/components/common/BaseSidebarGroup";
+import { useAppSelector } from "@/store";
+import {
+  createLabel,
+  deleteLabel,
+  getAllLabels,
+  labelSelectors,
+  updateLabel,
+} from "@/store/label-Store";
+import { defaultLabelFormValues, labelFormSchema } from "@/types/label";
+
+const LabelGroup = () => {
+  const labels = useAppSelector(labelSelectors.selectAll);
+
+  return (
+    <BaseSidebarGroup
+      entities={labels}
+      getAllEntities={getAllLabels}
+      createEntity={createLabel}
+      updateEntity={updateLabel}
+      deleteEntity={deleteLabel}
+      formSchema={labelFormSchema}
+      defaultFormValues={defaultLabelFormValues}
+      entityName="ラベル"
+    />
+  );
+};
+
+export { LabelGroup };
