f29ca55d2fdc0d5a5a349949327c18baf7ab618e
jfj2lk <parasol.sut.crt@gmail.com>
2025-07-08 16:55:22 +0900

    ♻️ user-storeの内容を修正。

--- vite/src/store/user-store.ts
+++ vite/src/store/user-store.ts
@@ -1,69 +1,50 @@
 import { RootState } from "@/store";
 import { userFormSchema, UserType } from "@/types/user";
-import {
-  createAsyncThunk,
-  createEntityAdapter,
-  createSlice,
-} from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { z } from "zod";
 
-// 全件取得
-export const getUser = createAsyncThunk("getUser", () => {
-  return axios.get("/api/auth/user").then((response) => response.data.data);
+// ユーザー取得
+export const getUser = createAsyncThunk("user/getUser", async () => {
+  const response = await axios.get("/api/auth/user");
+  return response.data.data;
 });
 
-// 作成
+// ユーザー作成
 export const createUser = createAsyncThunk(
-  "createUser",
-  (values: z.infer<typeof userFormSchema>) => {
-    return axios
-      .post("/api/auth/signup", values)
-      .then((response) => response.data.data);
+  "user/createUser",
+  async (values: z.infer<typeof userFormSchema>) => {
+    const response = await axios.post("/api/auth/signup", values);
+    return response.data.data;
   },
 );
 
-// 更新
+// ユーザー更新
 export const updateUser = createAsyncThunk(
-  "updateUser",
-  (payload: { id: number; values: z.infer<typeof userFormSchema> }) => {
-    return axios
-      .patch("/api/auth/user", payload.values)
-      .then((response) => response.data.data);
+  "user/updateUser",
+  async (payload: { id: number; values: z.infer<typeof userFormSchema> }) => {
+    const response = await axios.patch("/api/auth/user", payload.values);
+    return response.data.data;
   },
 );
 
-// 削除
-export const deleteUser = createAsyncThunk("deleteUser", (id: number) => {
-  return axios.delete("/api/auth/user").then((response) => response.data.data);
+// ユーザー削除
+export const deleteUser = createAsyncThunk("user/deleteUser", async () => {
+  await axios.delete("/api/auth/user");
+  return null;
 });
 
-const userAdapter = createEntityAdapter<UserType>();
-const initialState = userAdapter.getInitialState();
+// Slice
 export const userSlice = createSlice({
-  name: "users",
-  initialState,
+  name: "user",
+  initialState: null as UserType | null,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getUser.fulfilled, (state, action) => {
-      userAdapter.setAll(state, action);
-    });
-
-    builder.addCase(createUser.fulfilled, (state, action) => {
-      userAdapter.addOne(state, action);
-    });
-
-    builder.addCase(updateUser.fulfilled, (state, action) => {
-      userAdapter.setOne(state, action);
-    });
-
-    builder.addCase(deleteUser.fulfilled, (state, action) => {
-      userAdapter.removeOne(state, action);
-    });
+    builder
+      .addCase(getUser.fulfilled, (_, action) => action.payload)
+      .addCase(updateUser.fulfilled, (_, action) => action.payload);
   },
 });
 
 export const userReducer = userSlice.reducer;
-export const userSelectors = userAdapter.getSelectors(
-  (state: RootState) => state.user,
-);
+export const selectUser = (state: RootState) => state.user;
